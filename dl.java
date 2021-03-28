cf177a8c936c028c0dcb4590f65892217ea8b9b7
CoffeeCoder1 <76572535+CoffeeCoder1@users.noreply.github.com>
2021-03-28 11:21:57 -0400

    Create dl.java

---
+++ dl.java
@@ -0,0 +1,13 @@
+/* 
+ * Downloader tool
+ * Made by CoffeeCoder1@Github
+ */
+
+public class dl {
+  public static void main(String[] args) {
+    System.out.println("CoffeeCoder1's Downloader Toolkit");
+    System.out.println("Cross platform HTTP tools");
+  }
+  public static void dl(String path) {
+    System.out.println("Downloading " + path + "...");
+  }
